25d17a402cc1120cb1566c6e318374cbbb52d6f1
Stig-Ørjan Smelror <smelror@gmail.com>
2025-11-27 21:09:09 +0100

    chore: remove dist folder from git tracking

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Tstlai } from './core/Tstlai';
-export type { TranslationConfig, ProcessedPage, AIProvider } from './types';
-export * as integrations from './integrations';
-//# sourceMappingURL=index.d.ts.map
--- dist/providers/BaseAIProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import { AIProvider } from '../types';
-export declare abstract class BaseAIProvider implements AIProvider {
-    abstract translate(texts: string[], targetLang: string): Promise<string[]>;
-    abstract getModelInfo(): {
-        name: string;
-        capabilities: string[];
-    };
-}
-//# sourceMappingURL=BaseAIProvider.d.ts.map
